85f55705e85b4b387b50e4bea8787e50a0ee61f7
montlak <jonatthan91@gmail.com>
2018-01-28 09:40:40 -0700

    add termometro

---
+++ Assets/scripts/termometro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class termometro : MonoBehaviour {
+
+	public GameObject termometroBar;
+	public float amount = 0;
+	public float initial = 0.6230746f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		reduceBar (amount);
+	}
+
+	public void reduceBar(float value) {
+		Vector3 theScale = termometroBar.transform.localScale;
+		value = (initial / 100) * value;
+		theScale.x = value;
+
+		termometroBar.transform.localScale = theScale;
+	}
+}
